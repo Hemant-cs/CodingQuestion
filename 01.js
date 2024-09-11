9fecdbfe547b054b0a0a9ba0deecc08981f45edb
Hemant-cs <hemantchaurasia555@gmail.com>
2024-09-11 23:32:48 +0530

    uncommenting code

--- 01.js
+++ 01.js
@@ -14,16 +14,16 @@ const arr = [1,2,8,2,9,8];
 
 //method2 - more efficent
 
-// let obj = {...arr}; //{0:1, 1:2, 2:8, 3:2, 4:9, 5:8}
-// let uniqueProp = {};
+let obj = {...arr}; //{0:1, 1:2, 2:8, 3:2, 4:9, 5:8}
+let uniqueProp = {};
 
-// for(let key in obj){
-//     if(uniqueProp[obj[key]]>=0){
-//         uniqueProp[obj[key]] +=1;
-//     }else{
-//         uniqueProp[obj[key]]  = 1;
-//     }
-// }
+for(let key in obj){
+    if(uniqueProp[obj[key]]>=0){
+        uniqueProp[obj[key]] +=1;
+    }else{
+        uniqueProp[obj[key]]  = 1;
+    }
+}
 
-// console.log(uniqueProp) //{ '1': 1, '2': 2, '8': 2, '9': 1 }
+console.log(uniqueProp) //{ '1': 1, '2': 2, '8': 2, '9': 1 }
 
